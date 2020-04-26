f35c44d091f7e9746385f87358cef78dfea4d155
levtomer66 <levtomer66@gmail.com>
2020-04-26 03:17:01 +0300

    no fps

--- public/index.js
+++ public/index.js
@@ -3,31 +3,6 @@ var app = new Vue({
   el: '#app',
   data () {
     return {
-      fps: [
-        {
-          id: 0,
-          link: "/blog/from-top-down",
-          category: "Photography",
-          imgurl: "https://daks2k3a4ib2z.cloudfront.net/5604cdfeafeb72f96de8fbe6/5614ee7220fa552c1b077739_photo-1443926818681-717d074a57af.jpg",
-          title: "The Road Ahead",
-          description: "The road ahead might be paved - it might not be.",
-          authorimgurl: "https://daks2k3a4ib2z.cloudfront.net/5604cdfeafeb72f96de8fbe6/56150864b2f6f50c2d6ceada_128-14.jpg",
-          authorname: "Mat Vogels",
-          date: "September 25, 2015"
-        },
-        {
-          id: 1,
-          link: "/blog/from-top-daa",
-          category: "Photography",
-          imgurl: "https://daks2k3a4ib2z.cloudfront.net/5604cdfeafeb72f96de8fbe6/5614ee7220fa552c1b077739_photo-1443926818681-717d074a57af.jpg",
-          title: "The Road Ahead",
-          description: "The road ahead might be paved - it might not be.",
-          authorimgurl: "https://daks2k3a4ib2z.cloudfront.net/5604cdfeafeb72f96de8fbe6/56150864b2f6f50c2d6ceada_128-14.jpg",
-          authorname: "Mat Vogels",
-          date: "September 25, 2015"
-        }
-
-      ],
       posts: null
     }
   },
